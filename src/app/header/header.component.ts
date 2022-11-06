f694fb7e861d7fdd4c38c6fc70887964a2f1612e
AbdulArif <abdularif.dgp@gmail.com>
2022-11-06 13:37:27 +0530

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  menuType: String ='default'
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe((val: any) => {
+      if (val.url) {
+        console.warn(val.url)
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          console.log("In seller area")
+          this.menuType = "seller"
+        }
+        else {
+          console.log("OutSide seller area")
+          this.menuType = "default"
+        }
+      }
+    })
+  }
+
+  logOut(){
+    localStorage.removeItem('seller')
+    this.router.navigate(['/'])
   }
 
 }
