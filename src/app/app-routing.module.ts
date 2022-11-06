f662595aac7f6fdd017eeacf1a5b417f5ad6e5d4
AbdulArif <abdularif.dgp@gmail.com>
2022-11-06 14:40:53 +0530

    uo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { HomeComponent } from './home/home.component';
 import { NoPageComponent } from './no-page/no-page.component';
+import { SellerAddProductComponent } from './seller-add-product/seller-add-product.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path:'', component:HomeComponent },
   { path:'seller-auth', component:SellerAuthComponent },
   { path:'seller-home', component:SellerHomeComponent, canActivate:[AuthGuard] },
+  { path:'seller-add-product', component:SellerAddProductComponent, canActivate:[AuthGuard] },
   { path:'**', component:NoPageComponent },
   
 ];
