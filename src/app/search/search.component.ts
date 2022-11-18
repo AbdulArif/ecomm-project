6cecbcf04bb9406ae1bd0aa249026456dc424572
AbdulArif <abdularif.dgp@gmail.com>
2022-11-18 19:21:51 +0530

    update

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../data-type';
+import { ProductService } from '../sevices/product.service';
+
 
 @Component({
   selector: 'app-search',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  query!: string | null;
+  searchProducts!: Product[];
 
-  constructor() { }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.query = this.activatedRoute.snapshot.paramMap.get('query')
+    this.SearchProducts()
   }
 
+  SearchProducts() {
+    this.query && this.productService.SearchProducts(this.query).subscribe({
+      next: (res: any) => {
+        this.searchProducts = res
+        console.log(this.searchProducts)
+      },
+      error: (err) => { console.log(err) }
+    })
+  }
 }
