572bc7c2f5624d016d8594eb399c0381f555b5bb
AbdulArif <abdularif.dgp@gmail.com>
2022-11-20 23:18:10 +0530

    update

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../sevices/product.service';
+import { Product } from '../data-type';
+
 
 @Component({
   selector: 'app-product-details',
@@ -8,12 +11,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
 productId = '' 
+productData!: Product
+
   constructor(
-    private activatedRoute : ActivatedRoute
+    private activatedRoute : ActivatedRoute,
+    private productService : ProductService
   ) { }
 
   ngOnInit(): void {
     this.productId = this.activatedRoute.snapshot.paramMap.get('productId') as string
+    this.GetProductById()
   }
 
+  GetProductById() {
+    this.productService.GetProductById(this.productId).subscribe({
+      next: (res: any) => {
+        this.productData = res[0]
+        console.log(this.productData)
+      },
+      error: (err) => { console.log(err) }
+    })
+  }
 }
