e7a8f8db88d228be0d75733adf670bcdfb0dec3f
AbdulArif <abdularif.dgp@gmail.com>
2022-11-27 16:26:25 +0530

    update

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-auth',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-auth.component.scss']
 })
 export class UserAuthComponent implements OnInit {
+  userForm!: FormGroup;
 
-  constructor() { }
+  todaysDate: Date = new Date();
+
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.buildCreateUserForm();
+  }
+
+  buildCreateUserForm() {
+    this.userForm = this.formBuilder.group({
+      // userId: null,
+      name: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+      email: ['', [Validators.required, Validators.email]],
+      // addedBy: this.authenticationService.currentUserFirstName() + " " + this.authenticationService.currentUserLastName(),
+      addedDate: this.todaysDate.toISOString(),
+      // updatedBy: this.authenticationService.currentUserFirstName() + " " + this.authenticationService.currentUserLastName(),
+      updatedDate: this.todaysDate.toISOString()
+    });
+  }
+
+  onSubmit() {
+    console.log(this.userForm.value)
   }
 
 }
